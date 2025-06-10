435355afc3700ddee6c3d755712119839702b5ef
Jacktenor <53983732+jacktenor@users.noreply.github.com>
2025-06-09 23:28:25 -0400

    Add files via upload

--- installerworker.h
+++ installerworker.h
@@ -9,10 +9,6 @@ class InstallerWorker : public QObject {
 public:
     explicit InstallerWorker(QObject *parent = nullptr);
     void setDrive(const QString &drive);
-    void setUserInfo(const QString &username,
-                     const QString &password,
-                     const QString &rootPassword,
-                     const QString &desktopEnv);
 
 signals:
     void logMessage(const QString &message);
@@ -21,19 +17,9 @@ signals:
 
 public slots:
     void run();
-    void installUserAndDesktop();
 
 private:
     QString selectedDrive;
-    void mountPartitions();
-    void mountISO();
-    void bindSystemDirectories();
-    void installArchBase();
-    void installGrub();
-    QString selUser;
-    QString selPass;
-    QString selRootPass;
-    QString selDesktop;
 };
 
 #endif // INSTALLERWORKER_H
