d1257f454b9307b8997428b178bb8bc30b49e0c5
Jacktenor <53983732+jacktenor@users.noreply.github.com>
2025-06-14 20:40:34 -0400

    Use single ext4 partition for BIOS install

--- installerworker.cpp
+++ installerworker.cpp
@@ -21,13 +21,20 @@ void InstallerWorker::run() {
     process.waitForFinished();
     process.start("sudo", {"umount", "-l", "/mnt"});
     process.waitForFinished();
+    process.start("/bin/bash", {"-c",
+                                QString("lsblk -nr -o MOUNTPOINT /dev/%1").arg(selectedDrive)});
+    process.waitForFinished();
+    QStringList mps = QString(process.readAllStandardOutput()).split('\n', Qt::SkipEmptyParts);
+    for (const QString &mp : mps) {
+        process.start("sudo", {"umount", "-f", mp.trimmed()});
+        process.waitForFinished();
+    }
 
     // Partition
     emit logMessage("Creating new partition table...");
     QStringList cmds = {
         QString("sudo parted /dev/%1 --script mklabel msdos").arg(selectedDrive),
-        QString("sudo parted /dev/%1 --script mkpart primary ext4 1MiB 100%").arg(selectedDrive),
-        QString("sudo parted /dev/%1 --script set 1 boot on").arg(selectedDrive)
+        QString("sudo parted /dev/%1 --script mkpart primary ext4 1MiB 100%").arg(selectedDrive)
     };
     for (const QString &cmd : cmds) {
         process.start("/bin/bash", {"-c", cmd});
@@ -41,14 +48,15 @@ void InstallerWorker::run() {
     // Refresh table
     emit logMessage("Refreshing partition table...");
     process.start("/bin/bash", {
-        "-c", QString("sudo partprobe /dev/%1 && sudo udevadm trigger").arg(selectedDrive)
+        "-c",
+        QString("sudo partprobe /dev/%1 && sudo udevadm settle").arg(selectedDrive)
     });
     process.waitForFinished();
 
     // Format partition
     emit logMessage("Formatting partition " + rootPart + " as ext4...");
     process.start("/bin/bash", {
-        "-c", QString("sudo mkfs.ext4 -F -L ROOT %1").arg(rootPart)
+        "-c", QString("sudo mkfs.ext4 -F %1").arg(rootPart)
     });
     process.waitForFinished();
     if (process.exitCode() != 0) {
@@ -57,8 +65,10 @@ void InstallerWorker::run() {
     }
 
     // Mount
-    emit logMessage("Mounting partition...");
+    emit logMessage("Mounting partitions...");
     process.start("sudo", {"mount", rootPart, "/mnt"});
+    process.waitForFinished();
+    process.start("sudo", {"mkdir", "-p", "/mnt/boot"});
     process.waitForFinished();
 
     emit logMessage("✅ Drive is ready.");
