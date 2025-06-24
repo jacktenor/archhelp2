92aac1a04a7792546e3b3ea1be5dab5480b9d8a7
Jacktenor <53983732+jacktenor@users.noreply.github.com>
2025-06-23 23:14:39 -0400

    Add automatic privilege escalation via pkexec

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include "Installwizard.h"
 #include <QApplication>
 #include <QMessageBox>
+#include <QProcess>
+#include <QFileInfo>
 #include <unistd.h>
 
 int main(int argc, char *argv[]) {
@@ -8,6 +10,15 @@ int main(int argc, char *argv[]) {
 
     // Ensure the installer has the necessary privileges to run
     if (geteuid() != 0) {
+        // Try to relaunch the application using pkexec which will
+        // display a password prompt if needed.
+        QStringList args;
+        args << QFileInfo(argv[0]).absoluteFilePath();
+
+        if (QProcess::startDetached("pkexec", args)) {
+            return 0; // Child process launched; exit current instance
+        }
+
         QMessageBox::critical(nullptr, "Permissions Error",
                              "This installer must be run as root.\n"
                              "Please restart it using 'sudo' or 'pkexec'.");
