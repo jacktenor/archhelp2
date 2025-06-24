51f33a2adae3b8a054ca965e9b194d7bf59e7e20
Jacktenor <53983732+jacktenor@users.noreply.github.com>
2025-06-24 01:02:27 -0400

    Fix pkexec relaunch

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include "Installwizard.h"
 #include <QApplication>
 #include <QMessageBox>
+#include <QFileInfo>
+#include <errno.h>
 #include <unistd.h>
 
 int main(int argc, char *argv[]) {
@@ -8,6 +10,12 @@ int main(int argc, char *argv[]) {
 
     // Ensure the installer has the necessary privileges to run
     if (geteuid() != 0) {
+        // Try to relaunch the application using pkexec which will
+        // display a password prompt if needed.
+        QString path = QFileInfo(argv[0]).absoluteFilePath();
+        execlp("pkexec", "pkexec", path.toUtf8().constData(), (char*)nullptr);
+        // If execlp returns, it failed
+
         QMessageBox::critical(nullptr, "Permissions Error",
                              "This installer must be run as root.\n"
                              "Please restart it using 'sudo' or 'pkexec'.");
