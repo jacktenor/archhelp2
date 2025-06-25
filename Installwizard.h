2b249fdecab7c314700f61a992dc14856625a3f9
Jacktenor <53983732+jacktenor@users.noreply.github.com>
2025-06-24 23:06:01 -0400

    Fix drive variable usage

--- Installwizard.h
+++ Installwizard.h
@@ -25,6 +25,7 @@ private:
     QProgressBar *progressBar;
     QNetworkAccessManager *networkManager;
     QString selectedDrive;  // 🧠 TRACK THE CURRENT DRIVE
+    bool efiInstall = false; // track chosen boot mode
     QString getUserHome();
     void populateDrives(); // Populate the dropdown with available drives
     void downloadISO(QProgressBar *progressBar);
@@ -36,11 +37,13 @@ private:
     QStringList getAvailableDrives();        // Detect available drives
     void prepareDrive(const QString &drive);   // Prepare the selected drive
     void populatePartitionTable(const QString &drive); // new
-    void createDefaultPartitions(const QString &drive); // new example
+    void prepareForEfi(const QString &drive); // use free space for EFI
     void mountPartitions(const QString &drive);
     void mountISO();
     void installArchBase(const QString &selectedDrive);
     void installGrub(const QString &drive);
+
+    void setWizardButtonEnabled(QWizard::WizardButton which, bool enabled);
 };
 
 #endif // INSTALLWIZARD_H
